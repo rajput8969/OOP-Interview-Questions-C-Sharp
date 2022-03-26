b6841e866ee50d57000413b94f921e3a8f80dfae
ANMOL <anmol999rajput@gmail.com>
2022-03-26 17:28:30 +0530

    Add project files.

---
+++ OOP-Interview-Questions-C Sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_Interview_Questions_C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Object Oriented Programming Interview Questions C#");
+        }
+    }
+}
